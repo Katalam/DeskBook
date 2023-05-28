266856cc7156f22e970f4f476d4a5bdee684fd85
Bruno Görß <39590058+Katalam@users.noreply.github.com>
2023-05-28 18:23:29 +0200

    test(table-controller): added table controller index method test

---
+++ tests/Unit/Controller/TableControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Reservation;
+use App\Models\Table;
+use App\Models\User;
+use Inertia\Testing\AssertableInertia as Assert;
+
+it('will return a 200 response on table index', function () {
+    $user = User::factory()->create();
+    Table::factory()
+        ->has(
+            Reservation::factory()
+                ->for($user)
+                ->count(3))
+        ->count(15)
+        ->create();
+
+    $response = $this->actingAs($user)->get(route('tables.index'));
+
+    $response->assertOk();
+
+    $response->assertInertia(fn (Assert $page) => $page
+        ->component('Tables/Index')
+        ->has('tables')
+        ->has('tables.data', 15, fn (Assert $page) => $page
+            ->has('id')
+            ->has('name')
+            ->has('location')
+            ->has('reserved')
+            ->has('reservations', 3, fn(Assert $page) => $page
+                ->has('id')
+                ->has('date')
+                ->has('user')
+            )
+        )
+    );
+})->group('unit', 'controller', 'table-reservation');
