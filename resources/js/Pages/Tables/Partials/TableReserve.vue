f2aa6b7679b19fe16d16bd25139098be0b8360e5
Katalam <bruno.goerss@hyperlink.eu>
2023-06-18 22:56:19 +0200

    fix style on empty features

--- resources/js/Pages/Tables/Partials/TableReserve.vue
+++ resources/js/Pages/Tables/Partials/TableReserve.vue
@@ -97,15 +97,15 @@ interface Props {
                 </div>
             </div>
         </div>
-        <div class="mt-2">
+        <div class="mt-2" v-if="table.features?.length > 0">
             <div @click="featuresOpen = !featuresOpen">
-                <div class="flex items-center gap-1 text-sm" v-show="!featuresOpen">
+                <div class="flex items-center gap-1 text-sm cursor-pointer" v-show="!featuresOpen">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                     </svg>
                     <span>Attributes</span>
                 </div>
-                <div class="flex items-center gap-1 text-sm" v-show="featuresOpen">
+                <div class="flex items-center gap-1 text-sm cursor-pointer" v-show="featuresOpen">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                     </svg>
