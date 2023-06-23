aa319dde0ca3e6f530195fbdf8bf4109fc1af262
Katalam <bruno.goerss@hyperlink.eu>
2023-06-23 21:43:19 +0200

    fix long text for personio token

---
+++ database/migrations/2023_06_23_194217_change_text_to_long_text_on_teams_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('teams', static function (Blueprint $table) {
+            $table->longText('personio_token')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('teams', static function (Blueprint $table) {
+            $table->text('personio_token')->change();
+        });
+    }
+};
