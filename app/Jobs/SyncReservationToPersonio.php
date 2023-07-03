e019c06c583c29cfd0794ae1229b6c6aab5cb451
Katalam <bruno.goerss@hyperlink.eu>
2023-07-03 12:12:31 +0200

    fix: increase tries of sync job

--- app/Jobs/SyncReservationToPersonio.php
+++ app/Jobs/SyncReservationToPersonio.php
@@ -15,6 +15,8 @@ class SyncReservationToPersonio implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public int $tries = 3;
+
     /**
      * Create a new job instance.
      */
